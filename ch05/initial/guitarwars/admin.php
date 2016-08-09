12291f3cd3cb20f4d0c567d7270fc7efbe1ed5b9
Adrenalin13 <lsd00666@rambler.ru>
2016-08-09 16:47:20 +0300

    форма удаления готова, не работает после подтверждения, выдает что ни одного рейтинга не выбрано для удаления

--- ch05/initial/guitarwars/admin.php
+++ ch05/initial/guitarwars/admin.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Гитарные войны - Высшие рейтинги Administration</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  <h2>Гитарные войны - Высшие рейтинги Administration</h2>
+  <p>Ниже список всех рекордов Гитарных войн. Используйте эту страницу, чтобы удалить рейтинги по мере необходимости.</p>
+  <hr />
+
+
 <?php
 
 require_once('appvars.php');
@@ -13,14 +27,20 @@ $data = mysqli_query($dbc, $query);
 // Извлечение данных из массива рейтингов в цикле. Формирование данных записей в виде HTML
 echo '<table>';
 while ($row = mysqli_fetch_array($data)) {
-	// вывод данных рейтинга
-	echo '<tr class="scorerow"><td><strong>' . $row['name'] . '</strong></td>';
-	echo '<td>' . $row['date'] . '</td>';
-	echo '<td>' . $row['score'] . '</td>';
-	echo '<td><a href="removescore.php?id=' . $row['id'] . '&amp;date=' . $row['date'] . '&amp;name=' . $row['name'] . '&amp;score=' . $row['score'] . '&amp;screenshot=' . $row['screenshot'] . '">Удалить</a></td></tr>';
+    // вывод данных рейтинга
+    echo '<tr class="scorerow"><td><strong>' . $row['name'] . '</strong></td>';
+    echo '<td>' . $row['date'] . '</td>';
+    echo '<td>' . $row['score'] . '</td>';
+    echo '<td><a href="removescore.php?id=' . $row['id'] . '&amp;date=' . $row['date'] .
+      '&amp;name=' . $row['name'] . '&amp;score=' . $row['score'] .
+      '&amp;screenshot=' . $row['screenshot'] . '">Удалить</a></td></tr>';
 }
-	echo '</table>';
+    echo '</table>';
 
-	mysqli_close($dbc);
+    mysqli_close($dbc);
 
 ?>
+
+
+</body> 
+</html>
--- ch05/initial/guitarwars/removescore.php
+++ ch05/initial/guitarwars/removescore.php
@@ -1,5 +1,70 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<head>
+    <meta http-equiv="Content-Type" content="text/html"; charset="utf-8"/>
+    <title>Гитарные войны. Удаление рейтинга</title>
+</head>
+<body>
+    <h2>Гитарные войны. Удаление рейтинга</h2>
+
+
+
 <?php
 
+require_once('appvars.php');
+require_once('connectvars.php');
+
+if (isset($_GET['id']) && isset($_GET['date']) && isset($_GET['name']) && isset($_GET['score']) && isset($_GET['screenshot'])) {
+    // извлечение данных рейтинга из суперглобального массива $_GET
+    $id = $_GET['id'];
+    $date = $_GET['date'];
+    $name = $_GET['name'];
+    $score = $_GET['score'];
+    $screenshot = $_GET['screenshot'];
+} elseif (isset($_POST['id']) && isset($_POST['name']) && isset($_POST['score'])) {
+    // извлечение данных рейтинга из суперглобального массива $_POST
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $score = $_POST['score'];
+} else {
+    echo '<p class="error">Ни одного рейтинга не выбрано для удаления!</p>';
+}
+
+if (isset($_POST['submit'])) {
+    if ($_POST['confirm'] == 'Да') {
+        // Удаление с сервера файла изображения, подтверждающего рейтинг
+        @unlink(GW_UPLOADPATH . $screenshot);
+
+        // соединение с БД
+        $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        // удаление рейтинга из БД
+        $query = "DELETE FROM guitarwars WHERE id = $id LIMIT 1;";
+        mysqli_query($dbc, $query);
+        mysqli_close($dbc);
+
+        // Вывод пользователю страницы подтверждения
+        echo '<p>Рейтинг со значением ' . $score . ' для пользователя ' . $name . ' был успешно удален из Базы Данных.</p>';
+    } else {
+        echo '<p class="error">Рейтинг не удален...</p>';
+    }
+} elseif (isset($id) && isset($name) && isset($date) && isset($score)) {
+    echo '<p>Вы уверены что желаете удалить данный рейтинг?</p>';
+    echo '<p><strong>Имя: </strong>' . $name . '<br><strong>Дата: </strong>' . $date . '<br><strong>Рейтинг: </strong>' . $score . '</p>';
+    echo '<form metod="POST" action="removescore.php">';
+    echo '<input type="radio" name="confirm" value="Да"/>Да';
+    echo '<input type="radio" name="confirm" value="Нет" checked="checked"/>Нет<br>';
+    echo '<input type="submit" value="Удалить" name="submit"/>';
+    echo '<input type="hidden" name="id" value="' . $id . '"/>';
+    echo '<input type="hidden" name="name" value="' . $name . '"/>';
+    echo '<input type="hidden" name="score" value="' . $score . '"/>';
+    echo '</form>';
+}
+
+    echo '<p><a href="admin.php">&lt;&lt; Назад к списку рейтингов </a></p>';
 
 
 ?>
+
+
+</body>
+
+</html>
